f73c070d12a684e9f75d1389e02457baaac3743a
JCSC79 <jcsandomingo@gmail.com>
2025-03-24 16:30:56 +0100

    Primer commit de JC

---
+++ src/App.java
@@ -0,0 +1,7 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("\tHola con GitHub");
+        System.out.println("\t===============");
+        System.out.println("Probando JC....");
+    }
+}
